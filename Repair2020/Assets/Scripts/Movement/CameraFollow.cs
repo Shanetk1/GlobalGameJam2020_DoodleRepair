b1699647b74484209539ea8cab7b1428e63016bc
Shanetk1 <57373030+Shanetk1@users.noreply.github.com>
2020-02-03 07:55:41 -0500

    Release_

---
+++ Repair2020/Assets/Scripts/Movement/CameraFollow.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    CloudDrift CloudDrift;
+    public Transform CenterBackground;
+    public GameObject Player01;
+    public GameObject Player02;
+    float DistanceBetweenPlayers;
+    bool StartCheckPoint;
+    GameObject Target;
+    /** Camera has 2 anchors.
+        * MidAnchor - Shows middle of sreen by calculating DistanceBetweenPlayers.
+        *           - Anchor for Borders and CloudSpawnPoints.
+        * BotAnchor - OnTriggerExit (for death) collider with lowest point Player can fall.
+     ***/
+    [Header("Infinite clouds background")]
+    [SerializeField] GameObject[] Clouds = null;
+    [SerializeField] GameObject[] CloudsSpawnPoint = null;
+
+    void Start()
+    {   // CheckPoint is only used once. Eliminates (most) of camera jumpiness at start of game,
+        StartCheckPoint = false; // when transitioning between static and then following player.
+        transform.position = new Vector3(0, 1, -10f);   // Starting position of Camera.
+        Invoke("StartClouds", 10);
+    }
+
+    void Update()
+    {   // Remove Null Reference Error when destroyed Player GameObject.
+        try
+        {
+            LowestPlayer();
+            CameraFollowing();
+            BackgroundChanging();
+        }
+        catch { }
+    }
+
+    // Camera checks which Player is nearest bottom of screen, and switches between
+    void LowestPlayer() // Player01 or Player02.
+    {
+        if (Player01.transform.position.y <= Player02.transform.position.y)
+        {
+            DistanceBetweenPlayers = Player02.transform.position.y - Player01.transform.position.y;
+            Target = Player01;
+        }
+        else if (Player02.transform.position.y <= Player01.transform.position.y)
+        {
+            DistanceBetweenPlayers = Player01.transform.position.y - Player02.transform.position.y;
+            Target = Player02;
+        }
+        if (DistanceBetweenPlayers >= 17) { StartCheckPoint = true; }
+    }
+
+    void CameraFollowing()
+    {
+        if (Target.transform.position.y >= 1.7 || StartCheckPoint == true || DistanceBetweenPlayers >= 17)
+        { transform.position = new Vector3(transform.position.x, Target.transform.position.y + DistanceBetweenPlayers / 2, transform.position.z); }
+
+        // Tracking Main Camera distance
+        if ((int)transform.position.y >= 50)
+        {   // Camera will detach and speed.
+                    
+                    // STILL NEED TO DO
+        }
+    }
+
+    void BackgroundChanging()
+    {   // Background switches when above a certain height, uses CenterBackground as anchor.
+        if (transform.position.y >= CenterBackground.position.y + 23f)
+        { CenterBackground.position = new Vector3(CenterBackground.position.x, transform.position.y + 23f, 10); }
+        // Background switches below a certain height, uses CenterBackground as anchor.
+        else if (transform.position.y <= CenterBackground.position.y - 23f)
+        { CenterBackground.position = new Vector3(CenterBackground.position.x, transform.position.y - 23f, 10); }
+    }
+
+    void StartClouds() { StartCoroutine(CloudsSpawning()); }
+    IEnumerator CloudsSpawning()
+    {   // Takes the MidAnchor and spawns randomly.
+        float RandomHeight = Random.Range((Target.transform.position.y - 7), (Target.transform.position.y + 7));
+        Instantiate(Clouds[Random.Range(0, 2)], /* x is chosen between 2 random spawn points*/
+            new Vector3(CloudsSpawnPoint[Random.Range(0, 2)].transform.position.x, RandomHeight, 9), Quaternion.identity);
+        yield return new WaitForSeconds(6);
+        StartCoroutine(CloudsSpawning());
+    }
+}
---
+++ Repair2020/Assets/Scripts/Movement/Player02Movement.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player02Movement : MonoBehaviour
+{
+
+    ManagerGame ManagerGame;
+    Rigidbody2D Rigidbody2D;
+    BoxCollider2D BoxCollider2D;
+    [SerializeField] bool Grounded;
+
+    //Animations
+    public Animator myAnim;
+
+    public float MoveSpeed = 10f;
+    public float JumpVelocity = 10f;
+    public float FallMultiplyer = 3.5f;
+    public float LowJumpMultiplyer = 2f;
+
+    private bool doubleJump = false; //Handled internally
+    private float horizMove = 0.0f;
+
+    private bool isFacingRight = true;
+
+    public bool getisGround() { return Grounded; }
+
+
+    void Start()
+    {
+        ManagerGame = GameObject.Find("ManagerGame").GetComponent<ManagerGame>();
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        BoxCollider2D = GetComponent<BoxCollider2D>();
+        Grounded = false;
+    }
+
+    void Update()
+    {   // Allowing movement when not dead.
+        if (ManagerGame.State != ManagerGame.Game.Dead)
+        {
+            Movement();
+            BetterJump();
+
+        }
+    }
+
+    // L+R with Vector2.
+    // Jump with Rigidbody2D. 
+    // Can only jump once when grounded.
+    // Rigidbody Z Constraint, locked.
+    void Movement()
+    {
+        horizMove = Mathf.Abs(Input.GetAxisRaw("HorizontalPlayer2") * MoveSpeed);
+        //^ Could be used for left right movement but I need it for speed since we used Translate
+        myAnim.SetFloat("Speed", horizMove);
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Grounded == false && doubleJump == true)
+        {
+            //Expend Double Jump!
+            Debug.Log("Double Jumping");
+            Rigidbody2D.velocity = Vector2.up * JumpVelocity;
+            doubleJump = false;
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * MoveSpeed * Time.deltaTime);
+
+            if (isFacingRight)
+            {
+                flip();
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+
+            if (!isFacingRight)
+            {
+                flip();
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Grounded == true && Rigidbody2D.velocity.y == 0.0f)
+        {
+            myAnim.SetBool("isJump", true);
+
+            Rigidbody2D.velocity = Vector2.up * JumpVelocity;
+            Grounded = false;
+        }
+
+
+        if (Grounded == true && Rigidbody2D.velocity.y == 0.0f)
+        {
+            myAnim.SetBool("isJump", false);
+        }
+
+
+
+    }
+
+    // Less floaty when jumping.
+    void BetterJump()
+    {
+        if (Rigidbody2D.velocity.y < 0)
+        {
+            Rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplyer - 1) * Time.deltaTime;
+        }
+        else if (Rigidbody2D.velocity.y > 0 && !Input.GetKey(KeyCode.UpArrow))
+        {   // Jump increases longer holds down Jump Button.
+            Rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplyer - 1) * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag == "Powerup")
+        {
+            Destroy(collision.collider.gameObject);
+            doubleJump = true;
+
+        }
+        if (collision.gameObject.tag == "Ground")
+        { Grounded = true; }
+
+        if (collision.gameObject.tag == "Platforms")
+        { Grounded = true; }
+    }
+
+    // When exits PlayArea, destroyed obj and changes GameState.
+    /*  void OnTriggerExit2D(Collider2D collision)
+      {
+          Debug.Log(collision.gameObject.tag);
+          if (collision.gameObject.tag != "Platforms")
+          {
+              Destroy(this.gameObject);
+              ManagerGame.State = ManagerGame.Game.Dead;
+          }
+      }*/
+
+    private void flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        //Basically flipping the character 
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
